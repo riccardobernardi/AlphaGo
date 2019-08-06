662971d49c538c433f192ba3bde79c7ce5575500
riccardobernardi <riccardo.bernardi@rocketmail.com>
2019-08-06 12:11:50 +0200

    mods

--- dlgo/goboard.py
+++ dlgo/goboard.py
@@ -1,4 +1,6 @@
 import copy
+
+from dlgo import zobrist
 from dlgo.gotypes import Player
 
 class Move():
@@ -25,14 +27,16 @@ class Move():
 class GoString():
     def __init__(self, color, stones, liberties):
         self.color = color
-        self.stones = set(stones)
-        self.liberties = set(liberties)
+        self.stones = frozenset(stones)
+        self.liberties = frozenset(liberties)
 
-    def remove_liberty(self, point):
-        self.liberties.remove(point)
+    def without_liberty(self, point):
+        new_liberties = self.liberties - {point}
+        return GoString(self.color, self.stones, new_liberties)
 
-    def add_liberty(self, point):
-        self.liberties.add(point)
+    def with_liberty(self, point):
+        new_liberties = self.liberties | {point}
+        return GoString(self.color, self.stones, new_liberties)
 
     def merged_with(self, go_string):
         assert go_string.color == self.color
@@ -58,6 +62,7 @@ class Board():
         self.num_rows = num_rows
         self.num_cols = num_cols
         self._grid = {}
+        self._hash = zobrist.EMPTY_BOARD
 
     def place_stone(self, player, point):
         assert self.is_on_grid(point)
@@ -83,11 +88,23 @@ class Board():
             new_string = new_string.merged_with(same_color_string)
         for new_string_point in new_string.stones:
             self._grid[new_string_point] = new_string
+
+        self._hash ^= zobrist.HASH_CODE[point, player]
+
         for other_color_string in adjacent_opposite_color:
-            other_color_string.remove_liberty(point)
+            replacement = other_color_string.without_liberty(point)
+            if replacement.num_liberties:
+                self._remove_string(other_color_string.without_liberty(point))
+            else:
+                self._remove_string(other_color_string)
+
         for other_color_string in adjacent_opposite_color:
             if other_color_string.num_liberties == 0:
                 self._remove_string(other_color_string)
+
+    def _replace_string(self, new_string):
+        for point in new_string.stones:
+            self._grid[point] = new_string
 
     def _remove_string(self, string):
         for point in string.stones:
@@ -96,8 +113,13 @@ class Board():
                 if neighbor_string is None:
                     continue
                 if neighbor_string is not string:
-                    neighbor_string.add_liberty(point)
+                    self._replace_string(neighbor_string.with_liberty(point))
             self._grid[point] = None
+
+            self._hash ^= zobrist.HASH_CODE[point, string.color]
+
+    def zobrist_hash(self):
+        return self._hash
 
     def is_on_grid(self, point):
         return 1 <= point.row <= self.num_rows and \
@@ -120,6 +142,13 @@ class GameState():
         self.board = board
         self.next_player = next_player
         self.previous_state = previous
+        if self.previous_state is None:
+            self.previous_states = frozenset()
+        else:
+            self.previous_states = frozenset(
+                previous.previous_states |
+                {(previous.next_player, previous.board.zobrist_hash())}
+            )
         self.last_move = move
 
     def apply_move(self, move):
@@ -164,13 +193,8 @@ class GameState():
             return False
         next_board = copy.deepcopy(self.board)
         next_board.place_stone(player, move.point)
-        next_situation = (player.other, next_board)
-        past_state = self.previous_state
-        while past_state is not None:
-            if past_state.situation == next_situation:
-                return True
-            past_state = past_state.previous_state
-        return False
+        next_situation = (player.other, next_board.zobrist_hash())
+        return next_situation in self.previous_states
 
     def is_valid_move(self, move):
         if self.is_over():
--- dlgo/utils.py
+++ dlgo/utils.py
@@ -14,7 +14,7 @@ def print_move(player, move):
         move_str = "resigns"
     else:
         move_str = "%s%d" % (COLS[move.point.col - 1], move.point.row)
-    print( "%s %s" % (player, move_str))
+    print("%s %s" % (player, move_str))
 
 
 def print_board(board):
@@ -26,3 +26,8 @@ def print_board(board):
             line.append(STONE_TO_CHAR[stone])
         print("%s%d %s" % (bump, row, ''.join(line)))
     print("   " + "  ".join(COLS[:board.num_cols]))
+
+def point_from_coords(coords):
+    col = COLS.index(coords[0])+1
+    row = int(coords[1:])
+    return gotypes.Point(row = row, col = col)
